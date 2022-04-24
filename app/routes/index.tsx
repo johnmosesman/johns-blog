059ad6e091b817559c5f41ba11993e30109e3687
John Mosesman <johnmosesman@gmail.com>
2022-04-24 16:07:48 -0500

    Add views

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,11 +6,10 @@ export default function Index() {
       <div className="mb-12">
         <h1 className="mb-2 text-5xl">Hi, I'm John</h1>
         <p className="mb-2">
-          I'm a software developer building apps from 0 to 1 in the web3 space
-          at{" "}
-          <Link to="https://zeroxone.xyz" className="underline">
+          I'm a software developer building in the web3 space at{" "}
+          <a href="https://zeroxone.xyz" className="underline">
             0x1.
-          </Link>
+          </a>
         </p>
         <p>I like helping others learn how to become a developer.</p>
       </div>
@@ -44,7 +43,7 @@ export default function Index() {
         <p className="mb-2 text-sm text-gray-700">32min &bull; 6.2k views</p>
       </a>
       <a
-        className="mb-8 block"
+        className="mb-4 block"
         href="https://www.youtube.com/watch?v=jV-lAT5jgx0"
         target="_blank"
         rel="noopener"
@@ -80,7 +79,28 @@ export default function Index() {
         <p className="font-bold">
           How to Become an Outstanding Junior Developer
         </p>
-        <p className="mb-2 text-sm text-gray-700">July 22, 2020</p>
+        <p className="mb-2 text-sm text-gray-700">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mr-1 inline-block h-4 w-4 text-gray-700"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+            />
+          </svg>
+          68.8k &bull; July 22, 2020
+        </p>
       </a>
       <a
         className="mb-8 block"
@@ -96,10 +116,31 @@ export default function Index() {
           How to prepare for a technical interview - tips and tricks to help you
           perform your best
         </p>
-        <p className="mb-2 text-sm text-gray-700">September 30, 2019</p>
+        <p className="mb-2 text-sm text-gray-700">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mr-1 inline-block h-4 w-4 text-gray-700"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+            />
+          </svg>
+          20.3k &bull; September 30, 2019
+        </p>
       </a>
       <a
-        className="mb-8 block"
+        className="mb-4 block"
         href="https://www.freecodecamp.org/news/practical-git-and-git-workflows/"
         target="_blank"
         rel="noopener"
@@ -111,7 +152,28 @@ export default function Index() {
         <p className="font-bold">
           How to Use Git and Git Workflows – a Practical Guide
         </p>
-        <p className="mb-2 text-sm text-gray-700">April 1, 2021</p>
+        <p className="mb-2 text-sm text-gray-700">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mr-1 inline-block h-4 w-4 text-gray-700"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+            />
+          </svg>
+          37.8k &bull; April 1, 2021
+        </p>
       </a>
       <Link className="mb-12 block underline" to="writings">
         View all writings
